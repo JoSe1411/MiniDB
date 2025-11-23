1715306694f317e9a511ea16b164ecfb9c8600e4
JoSe1411 <joysen.3002@gmail.com>
2025-11-23 10:45:48 +0530

    Implemented skeleton for execution of commands.

--- db.cpp
+++ db.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<sstream>
 #include<vector>
 #include<algorithm>
 using namespace std;
@@ -7,6 +8,27 @@ using namespace std;
 struct InputBuffer{
     string buffer;
     ssize_t input_length;
+};
+
+enum MetaCommandResults{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+};
+
+enum PrepareResult{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_COMMAND
+};
+
+enum StatementType{
+    STATEMENT_INSERT,
+    STATEMENT_SELECT,
+    STATEMENT_WHERE,
+    STATEMENT_HAVING
+};
+
+struct Statement{
+    StatementType statement;
 };
 
 InputBuffer* new_input_buffer(){
@@ -33,25 +55,82 @@ void close_input_buffer(InputBuffer* reader){
 }
 
 
+void execute_command(Statement* statement); // Forward declaration
+
+PrepareResult prepare_statement(InputBuffer* reader,Statement* statement){    
+    stringstream ss(reader->buffer);
+    string command_type;
+    ss >> command_type;
+
+    if(command_type == "insert"){
+        statement->statement = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if(command_type == "select"){
+        statement->statement = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_COMMAND;
+}
+
+
+MetaCommandResults execute_meta_commands(InputBuffer* reader){
+    if(reader->buffer == ".exit"){ 
+        close_input_buffer(reader);
+        exit(0);
+    }
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+}
+
+void execute_command(Statement* statement){
+        switch(statement->statement){
+            case STATEMENT_INSERT:
+                cout<<"This is just a stub for the insert command."<<endl;
+                break;
+
+            case STATEMENT_SELECT:
+                cout<<"This is just a stub for the select command."<<endl;
+                break;
+            
+            default:
+                cout<<"Unrecognized command"<<endl;
+                break;
+        }
+}
+
+
 int main (){
     InputBuffer* input_buffer = new_input_buffer();
-    string user_data = "";
     while(true){
         print_prompt();
         read_input(input_buffer);
-        user_data += input_buffer->buffer;
-        if(input_buffer->buffer == ".exit"){
-            close_input_buffer(input_buffer);
-            break;
+
+        if (input_buffer->buffer.empty()) continue;
+
+        if (input_buffer->buffer[0] == '.') {
+            switch (execute_meta_commands(input_buffer)) {
+                case META_COMMAND_SUCCESS:
+                    continue;
+                case META_COMMAND_UNRECOGNIZED_COMMAND:
+                    cout << "Unrecognized command '" << input_buffer->buffer << "'" << endl;
+                    continue;
+            }
         }
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case PREPARE_SUCCESS:
+                break;
+            case PREPARE_UNRECOGNIZED_COMMAND:
+                cout << "Unrecognized keyword at start of '" << input_buffer->buffer << "'." << endl;
+                continue;
+        }
+
+        execute_command(&statement);
+        cout << "Executed." << endl;
     }
 
-    cout<<"User text  - "<<endl;
-    cout<<user_data<<endl;
-
     return 0;
-
-
 }
 
     
